c8d051547bad5cae222a3e3bfe95962c12efc9dd
trevinshu <trevinshu2008@gmail.com>
2022-07-29 02:56:09 -0600

    Finished Chapter 6 : Styling React Components

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './CourseGoalItem.css';
 
-const CourseGoalItem = props => {
+const CourseGoalItem = (props) => {
   // const [deleteText, setDeleteText] = useState('');
 
   const deleteHandler = () => {
